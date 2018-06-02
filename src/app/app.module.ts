8dfafda6b5300865c62997fb3dbb9437c5f2281c
perceptron007 <dprophecyguy@gmail.com>
2018-06-02 23:42:54 +0530

    Add-Card component created and Details Page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { WeatherComponent } from './ui/weather/weather.component';
+import { AddCardComponent } from './ui/add-card/add-card.component';
+import { DetailsComponent } from './pages/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    WeatherComponent,
+    AddCardComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ui/weather/weather.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
